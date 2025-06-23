aaab171ada5bbd002a8ae117241c6aad5acd06e6
erskinenyongesawanami <erskineviper@gmail.com>
2025-06-23 03:07:29 +0300

    add changes made

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution here
+// 1. Case-insensitive full match
+function findMatching(drivers, query) {
+  return drivers.filter(driver => 
+    driver.toLowerCase() === query.toLowerCase()
+  );
+}
+
+// 2. Case-sensitive "starts with" match
+function fuzzyMatch(drivers, query) {
+  return drivers.filter(driver =>
+    driver.startsWith(query)
+  );
+}
+
+// 3. Match by object property "name"
+function matchName(drivers, query) {
+  return drivers.filter(driver =>
+    driver.name === query
+  );
+}
